e425da5f3b272c1413f84f4fbbb86eef9b5b7187
Jan Sziegaud <j.sziegaud@atlan-tec.com>
2017-05-23 13:00:24 +0200

    progress

--- haskellbook/code/ex07.hs
+++ haskellbook/code/ex07.hs
@@ -14,3 +14,21 @@ nums x =
     LT -> -1
     GT -> 1
     _ -> 0
+
+tensDigit :: Integral a => a -> a
+tensDigit x = d
+  where xLast = x `div` 10
+        d = xLast `mod` 10
+
+tensDivmod :: Integral a => a -> a
+tensDivmod x = mod (fst (divMod x 10)) 10
+
+hunsD x = mod (fst (divMod x 100)) 100
+
+foldBool :: a -> a -> Bool -> a
+foldBool x y z
+  | z == True = x
+  | otherwise = y
+
+g :: (a -> b) -> (a, c) -> (b, c)
+g f (x, xs) = (f x, xs)
