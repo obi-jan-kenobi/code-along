d274688a25eef9f04f6739ff62501571f2a4d97d
Jan Sziegaud <j.sziegaud@gmail.com>
2017-10-26 22:46:33 +0200

    identity applicative

--- haskellbook/17_chapter/myidentity.hs
+++ haskellbook/17_chapter/myidentity.hs
@@ -3,4 +3,8 @@ newtype Identity a
     deriving (Eq, Show)
 
 instance Functor Identity where
-    fmap f Identity a = Identity $ f a
+    fmap f (Identity a) = Identity $ f a
+
+instance Applicative Identity where
+    pure a = Identity a
+    (<*>) (Identity f) (Identity a) = Identity $ f a
