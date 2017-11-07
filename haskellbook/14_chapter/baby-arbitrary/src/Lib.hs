75469c49a1cd7ea3a72116de1c91af4d7e820119
Jan Sziegaud <j.sziegaud@gmail.com>
2017-11-07 21:16:40 +0100

    chapter 14 - first exercise

--- haskellbook/14_chapter/baby-arbitrary/src/Lib.hs
+++ haskellbook/14_chapter/baby-arbitrary/src/Lib.hs
@@ -1,5 +1,6 @@
 module Lib where
 
+import GHC.Generics
 import Test.QuickCheck
 
 data Trivial
@@ -9,3 +10,17 @@ data Trivial
 data Identity a
     = Identity a
     deriving (Eq, Show)
+
+data Pair a b
+    = Pair a b
+    deriving (Eq, Show)
+
+data Sum a b
+    = First a
+    | Second b
+    deriving (Eq, Show)
+
+data Bool'
+    = True'
+    | False'
+    deriving (Generic)
--- haskellbook/14_chapter/baby-arbitrary/test/Spec.hs
+++ haskellbook/14_chapter/baby-arbitrary/test/Spec.hs
@@ -17,7 +17,30 @@ identityGen = do
 instance Arbitrary a => Arbitrary (Identity a) where
     arbitrary = identityGen
 
+pairGen :: (Arbitrary a, Arbitrary b) => Gen (Pair a b)
+pairGen = do
+    a <- arbitrary
+    b <- arbitrary
+    return $ Pair a b
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Pair a b) where
+    arbitrary = pairGen
+
+sumGen :: (Arbitrary a, Arbitrary b) => Gen (Sum a b)
+sumGen = do
+    a <- arbitrary
+    b <- arbitrary
+    oneof [return $ First a, return $ Second b]
+
+trueGen :: Gen Int
+trueGen = coarbitrary True' arbitrary
+
+falseGen :: Gen Int
+falseGen = coarbitrary False' arbitrary
+
 main :: IO ()
 main = do
     sample trivialGen
     sample (identityGen :: Gen (Identity Int))
+    sample (pairGen :: Gen (Pair Int String))
+    sample (sumGen :: Gen (Sum Char Int))
