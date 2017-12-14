8371165c96248dee05319fa0d8d2c890a3865cae
Jan Sziegaud <j.sziegaud@atlan-tec.com>
2017-12-14 17:11:20 +0100

    applicative progress

--- haskellbook/17_chapter/code-along.hs
+++ haskellbook/17_chapter/code-along.hs
@@ -53,3 +53,26 @@ mkPerson n a
 
 mkPerson' :: String -> String -> Maybe Person
 mkPerson' n a = Person <$> mkName n <*> mkAddress a
+
+
+data Cow = Cow
+    { name :: String
+    , age :: Int
+    , weight :: Int
+    } deriving (Eq, Show)
+
+noEmpty :: String -> Maybe String
+noEmpty "" = Nothing
+noEmpty str = Just str
+
+noNegative :: Int -> Maybe Int
+noNegative n    | n >= 0 = Just n
+                | otherwise = Nothing
+
+-- Validating to get rid of empty
+-- strings, negative numbers
+cowFromString :: String -> Int -> Int -> Maybe Cow
+cowFromString name' age' weight' =
+    Cow <$> noEmpty name'
+        <*> noNegative age'
+        <*> noNegative weight'
