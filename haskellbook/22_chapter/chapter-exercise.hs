19ddab2b56cf395518698284f2bbd1f24484afb9
Jan Sziegaud <j.sziegaud@gmail.com>
2018-05-27 17:51:59 +0200

    Reader (ch. 22)  chapter exercise start

---
+++ haskellbook/22_chapter/chapter-exercise.hs
@@ -0,0 +1,35 @@
+module ChapterExercise where
+
+import Control.Applicative
+import Data.Maybe
+
+x = [1, 2, 3]
+y = [4, 5, 6]
+z = [7, 8, 9]
+
+xs :: Maybe Integer
+xs = lookup 3 $ zip x y
+
+ys :: Maybe Integer
+ys = lookup 6 $ zip y z
+
+zs :: Maybe Integer
+zs = Nothing
+
+z' :: Integer -> Maybe Integer
+z' = flip lookup $ zip x z
+
+x1 :: Maybe (Integer, Integer)
+x1 = (,) <$> xs <*> ys
+
+x2 :: Maybe (Integer, Integer)
+x2 = (,) <$> ys <*> zs
+
+x3 :: Integer -> (Maybe Integer, Maybe Integer)
+x3 = (,) <$> z' <*> z'
+
+uncurry :: (a -> b -> c) -> (a, b) -> c
+uncurry f (x, y) = f x y
+
+summed :: Num c => (c, c) -> c
+summed = ChapterExercise.uncurry (+)
