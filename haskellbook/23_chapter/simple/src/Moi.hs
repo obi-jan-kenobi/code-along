2e6bb110a05c81a598731535e98dc9fc7a27958d
Jan Sziegaud <j.sziegaud@gmail.com>
2018-06-01 13:06:11 +0200

    State (ch.23) Moi

---
+++ haskellbook/23_chapter/simple/src/Moi.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE InstanceSigs #-}
+
+module Moi where
+
+import Control.Monad (join)
+
+newtype Moi s a = Moi { runMoi :: s -> (a, s) }
+
+instance Functor (Moi s) where
+  fmap :: (a -> b) -> Moi s a -> Moi s b
+  fmap f (Moi g) = Moi (\s -> (f (fst (g s)), s))
+
+instance Applicative (Moi s) where
+  pure :: a -> Moi s a
+  pure a = Moi (\s -> (a, s))
+
+  (<*>) :: Moi s (a -> b) -> Moi s a -> Moi s b
+  (Moi f) <*> (Moi g) = Moi (\s -> (fst (f s) (fst (g s)), s))
+
+instance Monad (Moi s) where
+  return = pure
+  (>>=) :: Moi s a -> (a -> (Moi s b)) -> Moi s b
+  (Moi f) >>= g = Moi (\s -> runMoi (g (fst (f s))) s)
--- haskellbook/23_chapter/simple/src/RandomExample2.hs
+++ haskellbook/23_chapter/simple/src/RandomExample2.hs
@@ -14,4 +14,41 @@ rollDie = state $ do
 -- state :: Monad m => (s -> (a, s)) -> StateT s m a
 
 rollDie' :: State StdGen Die
-rollDie' = intToDie <$> state $ randomR (1, 6)
+rollDie' = intToDie <$> state (randomR (1, 6))
+
+rollDieThreeTimes' :: State StdGen (Die, Die, Die)
+rollDieThreeTimes' = liftA3 (,,) rollDie rollDie rollDie
+
+-- repeat :: a -> [a]
+
+infiniteDie :: State StdGen [Die]
+infiniteDie = repeat <$> rollDie
+
+-- replicateM :: Monad m => Int -> m a -> m [a]
+
+nDie :: Int -> State StdGen [Die]
+nDie = flip replicateM rollDie
+
+rollsToGetTwenty :: StdGen -> Int
+rollsToGetTwenty = rollsToGetN 20
+
+rollsToGetN :: Int -> StdGen -> Int
+rollsToGetN n g = go 0 0 g
+  where
+    go sum count gen
+      | sum >= n = count
+      | otherwise =
+        let (die, nextGen) =
+              randomR (1, 6) gen
+        in go (sum + die)
+              (count + 1) nextGen
+
+rollsCountLogged :: Int -> StdGen -> (Int, [Die])
+rollsCountLogged n g = go 0 (0, []) g
+  where
+    go sum count gen
+      | sum >= n = count
+      | otherwise =
+        let (die, nextGen) =
+              randomR (1, 6) gen
+           in go (sum + die) (fst count + 1, snd count ++ [intToDie die]) nextGen
